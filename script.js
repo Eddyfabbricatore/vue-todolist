6ccd7acdc8331dbaf12864be9c260a9c8e268777
Eddyfabbricatore <eddyfabbricatore2@gmail.com>
2023-09-29 17:40:01 +0200

    js [add] addTask [add] removeTask
    html
    [add] v-if tasks.length

--- script.js
+++ script.js
@@ -15,8 +15,25 @@ createApp({
         {
         text: 'Ripassare Vue',
         done: false
-        },
-      ]
+        }
+      ],
+      newTask: '',
+      isError: false
+    }
+  },
+  methods:{
+    addTask(){
+      if(this.newTask.length < 5){
+        this.isError = true;
+      }else{
+        this.tasks.unshift({text: this.newTask, done: false});
+
+        this.newTask = '';
+        this.isError = false;
+      }
+    },
+    removeTask(i){
+      this.tasks.splice(i, 1);
     }
   }
 }).mount('#app');
